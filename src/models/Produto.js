4bc4f6a898ca7d44bb9e4c7b2cf7668e2efa7cc1
RootCryptDB <matheus.alucard.td@gmail.com>
2020-12-01 11:09:36 -0400

    Update Produto.js

--- src/models/Produto.js
+++ src/models/Produto.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const EstoqueSchema = new mongoose.Schema({
     produto  : String,
+    thumb : String,
+    desc : String,
     quantidade : Number,
     valor : Number,
     tipo : String
